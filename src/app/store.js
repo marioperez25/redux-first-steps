f8e92d8183844aa9c886c05d73e5c4ad632c4f42
marioperez25 <mariofph@gmail.com>
2020-10-13 09:15:06 -0600

    added test slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import componentTestSlice from '../features/firstSlice/componentTestSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    name: componentTestSlice,
   },
 });
