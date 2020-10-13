e7b47d3b2182d9f096868c9934d6637c187dda47
marioperez25 <mariofph@gmail.com>
2020-10-13 14:07:29 -0600

    added submitHandler, refactore useSelector

--- src/features/firstSlice/ComponentTest.js
+++ src/features/firstSlice/ComponentTest.js
@@ -2,27 +2,41 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
     changeName,
-    componentTestSelector,
   } from './componentTestSlice';
 
 export function ComponentTest(){
-    //const namePlaceholder = useSelector(componentTestSelector);//crashes app
-    const dispatch = useDispatch();
-    const [inputName, setInputName] = useState('');
+    const stateFirstName = useSelector(state => state.name.name);
+    const stateLastName = useSelector(state => state.name.lastName);
 
+    const dispatch = useDispatch();
+
+    const [inputFirstName, setinputFirstName] = useState('');
+    const [inputLastName, setInputLastName] = useState('');
+
+    const submitHandler = () => {
+        dispatch(
+            changeName([inputFirstName,inputLastName])
+        )
+    }
+        
     return(
         <div>
-            <label>New</label>
+            <label>Enter Name</label>
             <input
-                aria-label="Set name"
-                value={inputName}
-                onChange={e => setInputName(e.target.value)}
+                aria-label="Set firstname"
+                value={inputFirstName}
+                onChange={e => setinputFirstName(e.target.value)}
+            />
+            <input
+                aria-label="Set last name"
+                value={inputLastName}
+                onChange={e => setInputLastName(e.target.value)}
             />
             <button
-          aria-label="submit new name"
-          onClick={() => dispatch(changeName(inputName))}
-        >change it</button>
-            <span >"namePlaceholder"</span>
+                aria-label="submit new name"
+                onClick={submitHandler}
+            >change it</button>
+            <span >{stateFirstName} {stateLastName}</span>
         </div>
     )
 }
--- src/features/firstSlice/componentTestSlice.js
+++ src/features/firstSlice/componentTestSlice.js
@@ -3,17 +3,20 @@ import { createSlice } from '@reduxjs/toolkit';
 export const componentTest = createSlice({
     name: 'componentTest',
     initialState: {
-        name: 'test name'
+        name: '',
+        lastName: ''
     },
     reducers:{
         changeName: (state, action) => {
-            state.name = action.payload;
+            console.log(action.payload[1])
+            state.name = action.payload[0]
+            state.lastName = action.payload[1]
         },
     }
 })
 
 export const { changeName } = componentTest.actions;
 
-export const componentTestSelector = state => state.componentTest.name;
+export const componentTestSelector = state => state.componentTest;
 
 export default componentTest.reducer;
